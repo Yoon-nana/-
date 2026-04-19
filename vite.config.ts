77c8e2dd46d0140e5f87d9235e91ce5ba13abc28
Yoon-nana <ayoyo0613@gmail.com>
2026-04-20 00:24:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,17 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-    base: '/-/',
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+ 
+  base: '/-/', 
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
